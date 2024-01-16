89b2863aae549ba5fc22038ccf1b3f7732af6de0
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-01-16 12:14:21 -0600

    🖊 add dinamic breadcrump by path

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,7 +1,16 @@
+import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
+
 export function BreadcrumbSimple() {
+  const location = useLocation()
+    .pathname.split("/")
+    .slice(1)
+    .filter((x) => !parseInt(x));
+  const { t } = useTranslation();
+  const title = location[location.length - 1];
   return (
     <>
-      <nav aria-label="Breadcrumb" className="text-slate-800 dark:text-white" >
+      <nav aria-label="Breadcrumb" className="text-slate-800 dark:text-white">
         <ol className="flex items-center gap-1 text-sm ">
           <li>
             <a href="#" className="block transition hover:text-gray-700">
@@ -23,53 +32,40 @@ export function BreadcrumbSimple() {
               </svg>
             </a>
           </li>
-
-          <li className="rtl:rotate-180">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </li>
-
-          <li>
-            <a href="#" className="block transition hover:text-gray-700">
-              {" "}
-              Shirts{" "}
-            </a>
-          </li>
-
-          <li className="rtl:rotate-180">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </li>
-
-          <li>
-            <a href="#" className="block transition hover:text-gray-700">
-              {" "}
-              Plain Tee{" "}
-            </a>
-          </li>
+          {location.map((l, i) => {
+            return <BreadcrumbOption key={i} text={l} />;
+          })}
         </ol>
-        <h2 className="font-base">Plain Tee</h2>
+        <h2 className="font-base">{t(`breadcrumb.${title}`)}</h2>
       </nav>
     </>
   );
 }
+
+export const BreadcrumbOption = ({ text }: { text: string }) => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <li className="rtl:rotate-180">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </li>
+
+      <li>
+        <a href="#" className="block transition hover:text-gray-700">
+          {t(`breadcrumb.${text}`)}
+        </a>
+      </li>
+    </>
+  );
+};
