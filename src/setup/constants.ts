d22b9a927be1e24da5a60333678c29dbee9acaa8
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-21 15:09:46 -0600

    ➕#add: Environment variable to see the mode

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,18 +1,21 @@
 //* API
+export const ENVIRONMENT_MODE = import.meta.env.VITE_ENVIRONMENT_MODE;
 export const API_URL = import.meta.env.VITE_API_URL;
 export const ENDPOINT_LOGIN = import.meta.env.VITE_ENDPOINT_LOGIN;
 
 export const ENDPOINT_PRODUCTS = import.meta.env.VITE_ENDPOINT_PRODUCTS;
-export const ENDPOINT_PRODUCTS_STORE = import.meta.env.VITE_ENDPOINT_PRODUCTS_STORE;
+export const ENDPOINT_PRODUCTS_STORE = import.meta.env
+  .VITE_ENDPOINT_PRODUCTS_STORE;
 
 export const ENDPOINT_USOCFDI = import.meta.env.VITE_ENDPOINT_USOCFDI;
-export const ENDPOINT_REGIMENFISCAL = import.meta.env.VITE_ENDPOINT_REGIMENFISCAL;
+export const ENDPOINT_REGIMENFISCAL = import.meta.env
+  .VITE_ENDPOINT_REGIMENFISCAL;
 export const ENDPOINT_PAISES = import.meta.env.VITE_ENDPOINT_PAISES;
 export const ENDPOINT_ESTADO = import.meta.env.VITE_ENDPOINT_ESTADO;
 
 export const ENDPOINT_CATEGORIES = import.meta.env.VITE_ENDPOINT_CATEGORIES;
 
-export const ENDPOINT_BRAND = import.meta.env.VITE_ENDPOINT_BRAND
+export const ENDPOINT_BRAND = import.meta.env.VITE_ENDPOINT_BRAND;
 
 export const ENDPOINT_CUSTOMER = import.meta.env.VITE_ENDPOINT_CUSTOMER;
 
